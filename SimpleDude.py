42e148fc4feded60ba6c9357d8f8912d7f3e405e
vivekkuamrbagaria <vivekee047@gmail.com>
2015-10-08 18:00:43 -0700

    chaning the order of input variables

--- SimpleDude.py
+++ SimpleDude.py
@@ -564,23 +564,23 @@ StartTime = time.time()
 # From terminal
 
 if len( sys.argv )> 1 :
-    flipProbab = float( sys.argv[1] )
-else:
-    flipProbab = 0.01
-
-if len( sys.argv )> 2 :
-    SequenceLength = int( float( sys.argv[2] ) )
+    SequenceLength = int( float( sys.argv[1] ) )
 else:
     SequenceLength = int( 1e3 )
 
-if len( sys.argv )> 4:
+if len( sys.argv )> 2 :
+    flipProbab = float( sys.argv[2] )
+else:
+    flipProbab = 0.01
+
+if len( sys.argv )> 3:
     ContextLengthMin = int( sys.argv[3] )
 else:
     ContextLengthMin = 3
 
 
-if len( sys.argv )> 3:
-    ContextLengthMax = int( sys.argv[3] )
+if len( sys.argv )> 4:
+    ContextLengthMax = int( sys.argv[4] )
 else:
     ContextLengthMax = 4
 
