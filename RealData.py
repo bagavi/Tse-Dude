1b095e67e5d07f1d0b2a547ccf8038d6379ad440
vivekkuamrbagaria <vivekee047@gmail.com>
2015-10-11 17:33:55 -0700

    Adding new function

---
+++ RealData.py
@@ -0,0 +1,27 @@
+import time, SimpleDude, CommonFunctions, sys
+StartTime = time.time()
+# From terminal
+
+if len( sys.argv )> 1 :
+    flipProbab = float( sys.argv[1] )
+else:
+    flipProbab = 0.1
+
+if len( sys.argv )> 2:
+    ContextLengthMin = int( sys.argv[2] )
+else:
+    ContextLengthMin = 3
+
+
+if len( sys.argv )> 3:
+    ContextLengthMax = int( sys.argv[3] )
+else:
+    ContextLengthMax = 10
+
+
+
+
+
+Obj = SimpleDude.System( ContextLengthMin = ContextLengthMin, ContextLengthMax = ContextLengthMax, flipProbab=flipProbab, shouldIprint = False)
+Obj.mainRealData('..\original\genome_data.fasta')
+print( "total execution time", time.time() - StartTime)
