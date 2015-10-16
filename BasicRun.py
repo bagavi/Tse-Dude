d1ad85cf42abdb51c0c7ee81511b24bd23a79e88
vivekkuamrbagaria <vivekee047@gmail.com>
2015-10-16 11:04:12 -0700

    test

---
+++ BasicRun.py
@@ -0,0 +1,39 @@
+import time, SimpleDude, CommonFunctions, sys
+StartTime = time.time()
+# From terminal
+
+if len( sys.argv )> 1 :
+    SequenceLength = int( float( sys.argv[1] ) )
+else:
+    SequenceLength = int( 1e5 )
+
+if len( sys.argv )> 2 :
+    flipProbab = float( sys.argv[2] )
+else:
+    flipProbab = 0.01
+
+if len( sys.argv )> 3:
+    ContextLength = int( sys.argv[3] )
+else:
+    ContextLength = 1
+
+
+
+
+
+shouldIprint =  lambda x, y: False
+
+Obj = SimpleDude.System(  ContextLength = ContextLength , MarkovSequenceLength=SequenceLength, flipProbab=flipProbab, shouldIprint=shouldIprint)
+Obj.SimpleMain(.99)
+I = Obj.Input.Sequence
+C = Obj.Output.ReceivedSequence
+O = Obj.Output.Sequence
+# 
+# for i in range(len(I)):
+#     if ( I[i] != O[i] ):
+#         print( i )
+#         print( "In", I[i-3:i+4])
+#         print( "Re", C[i-3:i+4])
+#         print( "Ou", O[i-3:i+4], "\n")
+#         a= 10
+print( "total execution time", time.time() - StartTime)
