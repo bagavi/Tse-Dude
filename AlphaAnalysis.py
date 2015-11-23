3e9977986f7431d968209f42b2159f73a76fe136
vivekkuamrbagaria <vivekee047@gmail.com>
2015-11-22 21:10:36 -0800

    .

---
+++ AlphaAnalysis.py
@@ -0,0 +1,32 @@
+import CommonFunctions, numpy
+from operator import itemgetter
+
+def AlphaAnalysis( RContextLength = 8 ):
+    Array = CommonFunctions.FiletoArray('YTrueFakeResults_reads_simulation__posix.csv', Int=True)
+    Dictionary = dict()
+    ReadArray = []
+    for row in Array:
+        alpha = int(row[-1])
+        CoverageDepth = int(row[-3])
+        ContextLength = int(row[2])
+        key = (alpha, CoverageDepth, ContextLength )
+        NetCorrections = float(row[-4])
+        Changes = float( row[-6] )
+        WrongCorrections = float( row[-6]) - float(row[-5])
+        data = [NetCorrections, Changes, WrongCorrections]
+        Dictionary[ key ] = Dictionary.get( key, []) + [ data ]
+    
+    DataArray = []
+    for i in Dictionary:
+        DataArray += [ list( numpy.append( numpy.array(list(i))  , numpy.average(numpy.array( Dictionary[i] ), 0 ) ) )]
+    DataArray.sort( key = itemgetter(1) ) # Sorted w.r.t C D
+    DataArray.sort( key = itemgetter(0) ) # Sorted w.r.t Alpha
+    DataArray.sort( key = itemgetter(2) ) # Sorted w.r.t C.L
+    
+    FilteredDataArray = [  row for row in DataArray if(row[2] == RContextLength) ]
+    FilteredDataArray.sort(key=itemgetter(1), reverse=True)
+    for i in FilteredDataArray:
+        print(i)
+    print( "hi" )
+    
+AlphaAnalysis( )    
